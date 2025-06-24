71cc3bc3252a7d6d9f61bdb27a4733bf1094e40d
HemanthKasire <hr@enkonix.in>
2025-06-24 23:50:43 +0530

    Initial
    Basic Structure Commit

---
+++ esswordpress/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+  <h1>Welcome to the Intern WordPress Theme 🎉</h1>
+  <p>This theme is working. Start editing it in <code>wp-content/themes/my-custom-theme/</code></p>
+</main>
+
+<?php get_footer(); ?>
